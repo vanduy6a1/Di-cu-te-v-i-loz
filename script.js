7e38a0d61a58ba805d5663deb21d3f141c36bb70
vanduy6a1 <vanduy6a1@gmail.com>
2025-03-06 01:07:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let score = 0;
 let cross = true;
 let isPaused = false;
 let count = 0;
+let ok = false;
 
 const audio = new Audio('music.mp3');
 const audiogo = new Audio('gameover.mp3');
@@ -52,11 +53,12 @@ document.onkeydown = function (e) {
 
 // Kiểm tra va chạm
 setInterval(() => {
-    if (isPaused) return;
+    if (isPaused || ok) return;
 
     let dora = document.querySelector('.dora');
     let gameOver = document.querySelector('.gameOver');
     let obstacle = document.querySelector('.obstacle');
+    let bird = document.querySelector('.bird');
 
     let dx = parseInt(window.getComputedStyle(dora, null).getPropertyValue('left'));
     let dy = parseInt(window.getComputedStyle(dora, null).getPropertyValue('top'));
@@ -68,8 +70,14 @@ setInterval(() => {
     let offsetY = Math.abs(dy - oy);
 
     if (offsetX < 70 && offsetY < 50) {
+        ok = true;
         gameOver.innerHTML = "Game Over! Click Restart";
         obstacle.classList.remove('obstacleAni');
+        bird.classList.remove('birdlb');
+        bird.style.left = -100 + "px";
+        bird.style.top = -100 + "px";
+        dora.style.left = -100 + "px";
+        dora.style.top = -100 + "px";
         audiogo.play();
         setTimeout(() => {
             audiogo.pause();
@@ -86,7 +94,44 @@ setInterval(() => {
             let cute = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
         }
     }
+
+    // --------------------------------------------
+    if (isPaused || ok) return;
+
+    dx = parseInt(window.getComputedStyle(dora, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dora, null).getPropertyValue('top'));
+    ox = parseInt(window.getComputedStyle(bird, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(bird, null).getPropertyValue('top'));
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+
+    if (offsetX < 70 && offsetY < 50) {
+        ok = true;
+        gameOver.innerHTML = "Game Over! Click Restart";
+        obstacle.classList.remove('obstacleAni');
+        bird.classList.remove('birdlb');
+        bird.style.left = -100 + "px";
+        bird.style.top = -100 + "px";
+        dora.style.left = -100 + "px";
+        dora.style.top = -100 + "px";
+        audiogo.play();
+        setTimeout(() => {
+            audiogo.pause();
+            audio.pause();
+        }, 1000);
+    } else if (offsetX < 145 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+        if (score % 15 == 0) {
+            let cute = parseInt(window.getComputedStyle(bird, null).getPropertyValue('left'));
+        }
+    }
 }, 10);
+
 
 function updateScore(score) {
     document.getElementById('scoreCont').innerHTML = "Your Score: " + score;
@@ -97,6 +142,7 @@ document.getElementById('pauseBtn').addEventListener('click', function () {
     isPaused = true;
     audio.pause();
     document.querySelector('.obstacle').style.animationPlayState = 'paused';
+    document.querySelector('.bird').style.animationPlayState = 'paused';
 });
 
 // Chức năng Resume
@@ -104,6 +150,7 @@ document.getElementById('resumeBtn').addEventListener('click', function () {
     isPaused = false;
     audio.play();
     document.querySelector('.obstacle').style.animationPlayState = 'running';
+    document.querySelector('.bird').style.animationPlayState = 'running';
 });
 
 // Chức năng Restart
@@ -114,6 +161,7 @@ document.getElementById('restartBtn').addEventListener('click', function () {
 document.addEventListener("keydown", function (e) {
     if (e.key === "r" || e.key === "R") {
         location.reload();
+        ok = false;
     }
 });
 
@@ -132,3 +180,20 @@ document.addEventListener("keydown", function(e) {
         document.querySelector('.obstacle').style.animationPlayState = 'running';
     }
 });
+
+function randomPosition() {
+    let bird = document.querySelector(".bird");
+    let maxX = window.innerWidth - bird.clientWidth;
+    let maxY = window.innerHeight - bird.clientHeight;
+
+    let randomX = Math.floor(Math.random() * maxX);
+    let randomY = Math.floor(Math.random() * maxY);
+
+    bird.style.left = randomX + "px";
+    bird.style.top = randomY + "px";
+}
+
+setInterval(() => {
+    if (ok) return;
+    randomPosition();
+}, 1000); // Cập nhật vị trí mỗi giây
